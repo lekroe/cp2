f56d81e37847aaf8e204aaa87dc98dd5be63b93e
lekroe <s-lekroe@frontend.service>
2019-05-15 13:57:18 +0200

    umrechnung von fahrenheit in celsius und umgekehrt.  Please enter the commit message for your changes. Lines starting

---
+++ processing/CMakeFiles/bin_helloworld.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/bin_helloworld.dir/main.cpp.o"
+  "bin_helloworld.pdb"
+  "bin_helloworld"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/bin_helloworld.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ processing/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <fstream>
+
+int main(int argc, char** argv){
+    std::ifstream in("input.txt");
+
+  double var1;
+  int var4
+  double var2 = 9./5.;
+  double var3 = 32; 
+
+
+    for( int i = 0 ; i < 4; i++){
+    in >>var1;
+    in >>var4;
+
+    if (var4 = C)
+    std::cout << "Fahrenheit" << (var1*var2)+var3<< std::endl;
+
+    else (var4 = F)
+    std::cout << "Celsius" <<(var1-var3)/var2 << std:endl;
+    
+
+}
+
+  
+
+    return 0;
+}
