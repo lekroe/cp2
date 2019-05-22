e0cda6745b6a7b5ca558adec18769130ecc36263
lekroe <s-lekroe@frontend.service>
2019-05-22 13:00:05 +0200

    Umrechnung von Fahrenheit zu Celsius und umgekehrt. txt Datei mit 4 Einträgen erforderlich.

--- processing/main.cpp
+++ processing/main.cpp
@@ -5,7 +5,7 @@ int main(int argc, char** argv){
     std::ifstream in("input.txt");
 
   double var1;
-  int var4
+  std::string var4;
   double var2 = 9./5.;
   double var3 = 32; 
 
@@ -14,16 +14,14 @@ int main(int argc, char** argv){
     in >>var1;
     in >>var4;
 
-    if (var4 = C)
-    std::cout << "Fahrenheit" << (var1*var2)+var3<< std::endl;
-
-    else (var4 = F)
-    std::cout << "Celsius" <<(var1-var3)/var2 << std:endl;
-    
+    if (var4 == "C"){
+    std::cout << var1 << "Celsius in Fahrenheit" << (var1*var2)+var3<< std::endl;
+    }
+    else if (var4 == "F"){
+    std::cout << var1 << "Fahrenheit in Celsius" <<(var1-var3)/var2 << std::endl;
+    }
 
 }
-
-  
 
     return 0;
 }
