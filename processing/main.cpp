553fbbae6af32c7dd0db331e8dd878e4a4e45c97
lekroe <s-lekroe@frontend.service>
2019-05-22 13:52:05 +0200

    Input in Vektor einlesen und auswerfen. unvollständig. Klappt noch nicht

--- processing/main.cpp
+++ processing/main.cpp
@@ -1,25 +1,26 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
 
 int main(int argc, char** argv){
     std::ifstream in("input.txt");
+    std::vector<double> hector(4,0);
+    for( double& e : hector ){
+     in>>e;  
+}   	
 
   double var1;
   std::string var4;
   double var2 = 9./5.;
   double var3 = 32; 
 
-
-    for( int i = 0 ; i < 4; i++){
-    in >>var1;
-    in >>var4;
+    for( int i = 0 ; i < hector.size(); i++ ) {
+      
 
     if (var4 == "C"){
     std::cout << var1 << "Celsius in Fahrenheit" << (var1*var2)+var3<< std::endl;
     }
-    else if (var4 == "F"){
-    std::cout << var1 << "Fahrenheit in Celsius" <<(var1-var3)/var2 << std::endl;
-    }
+  
 
 }
 
