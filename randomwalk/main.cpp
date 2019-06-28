01d510c19050125c0a98fdc0bb4e75ba22e0a9a4
ich <suendermanneric@gmail.com>
2019-06-28 13:12:58 +0200

    fertiges programm. bugs gefixt. bereit zum plotten. x

--- randomwalk/main.cpp
+++ randomwalk/main.cpp
@@ -2,16 +2,16 @@
 #include <fstream>
 #include <vector>
 #include <random>
+//#include <ofstream>
 using namespace std;
 
 int main(int argc, char** argv){
-
     std::mt19937 gen;
-    gen.seed(5);
+    gen.seed(123);
     std::uniform_real_distribution<double> dis(0,1);
     std::vector<int> hector(10000000,0);
-    std::vector<double> timehector(10,0);
-
+    std::vector<double> timehector(11,0);
+std::vector<double> zahlort(2* timehector.size() +1,0);
     double a;
     double x1;
     double x1ave;
@@ -20,7 +20,7 @@ int main(int argc, char** argv){
     
 
 // Loop für Zeitschritte    
-    for (int i = 0; i < timehector.size(); ++i){
+    for (int i = 1; i < timehector.size(); ++i){
 
 // Loop für die Sprünge
       for (int n = 0; n < hector.size(); ++n){
@@ -32,26 +32,40 @@ int main(int argc, char** argv){
       hector[n]+=-1;}
       }
 // Loop Ende 
-
+//x1=0;
 // 1. Moment <x> ausrechnen 
       for (int n = 0; n < hector.size(); ++n){
       x1+= hector[n];
       }
 
       x1ave = x1*1/hector.size();
+      //cout << x1ave << endl;
 
-// 2. Moment <x^{2}> ausrechnen 
+// 2. Moment <x^{2}> ausrechnen
+x2=0;
       for (int n = 0; n < hector.size(); ++n){
-      x2+= hector[n]*hector[n]; 
+      x2+= (hector[n]-x1ave)*(hector[n]-x1ave); 
       }
 
       x2ave= x2*1/hector.size();
-
+   //   cout << x2ave << endl;
 // Momente über Zeit in Vektor reinschreiben
     timehector[i] = x2ave;
-    }
 
-    for(int t = 0; t < timehector.size(); t++){    
-    cout << timehector[t] << endl;
     }
+    for (int k=0; k < 2*timehector.size()+1; k++){
+
+	    for(int n=0; n < hector.size(); n++){
+		if (hector[n]==k-timehector.size()) {
+
+			zahlort[k]=zahlort[k]+1;
+		}
+	}
+cout << k << ":" << zahlort[k] << endl;
+	    
+
+    } 
+   // for(int t = 0; t < timehector.size(); t++){    
+   //  cout << timehector[t] << endl;
+  //  }
 }
