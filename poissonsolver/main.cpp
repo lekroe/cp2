70973cab4c841ac0bacaf956a24db9133d62a4ef
lekroe <s-lekroe@frontend.service>
2019-07-12 13:12:28 +0200

    Testwerte Poisson solver rho=0

--- poissonsolver/main.cpp
+++ poissonsolver/main.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-int main (){
+int main (int argc, char** argv){
 
 // Laengen des Gitters festlegen
   const int a = 50;
@@ -25,32 +25,28 @@ for (int i = 0; i < a; ++i){
 
 
   // Zeitschritte durch Abbruchbedingung
-double delta = 100;
-while (delta > 0.01){
+double delta = 10;
+while (delta > 0.1){
   
  //Raumelicher Loop, gehe Gitterpunkte durch 
   for (int i = 1; i < a-1; ++i){
     for (int j = 1; j < b-1; ++j){
       for (int k = 1; k < c-1; ++k){
-	phineu[i][j][k] = (1/6.)*(phialt[i+1][j][k] + phialt[i-1][j][k] + phialt[i][j+1][k] + phialt[i][j-1][k] + phialt[i][j][k+1] + phialt[i][j][k-1]); 
-      delta = sqrt((phineu[i][j][k] - phialt[i][j][k])*(phineu[i][j][k] - phialt[i][j][k]));
+	phineu[i][j][k] = (1.0/6.0)*(phialt[i+1][j][k] + phialt[i-1][j][k] + phialt[i][j+1][k] + phialt[i][j-1][k] + phialt[i][j][k+1] + phialt[i][j][k-1]); 
+      delta = sqrt((phialt[i][j][k] - phineu[i][j][k])*(phialt[i][j][k] - phineu[i][j][k]));
+      phialt[i][j][k] = phineu[i][j][k];
       }
     }
   }
 }        
 
-for (int i = 0; i < 50; ++i){
-     for (int j = 0; j < 50; ++j){
-        for (int k = 0; k < 50; ++k){
-	  cout<< i <<" "<< j <<" "<< k <<" "<<phialt[i][j][k]<< endl;
-	}
+// Ausgabe der Daten
+std::ofstream output_potential0("Potential_rho_0.txt");
+  for (int i = 0; i < 50; ++i){
+      for (int k = 0; k < 50; ++k){
+      output_potential0 << i <<" "<< k <<" "<<phineu[i][25][k]<< std::endl;
       }
-    }
-
-
-
+  }
 
 } 
-
-
 
