808ca759c423fdc2a72be91f50a0d82c1069b1ea
lekroe <s-lekroe@frontend.service>
2019-07-12 17:00:03 +0200

    Poissonsolver mit Punktladung in Arbeit. Output bis jetzt nur 0en. Wird morgen gefixt

--- poissonsolver/main.cpp
+++ poissonsolver/main.cpp
@@ -23,40 +23,41 @@ int main (int argc, char** argv){
 //rho=0////////////////////
 
 
-  // Randbedingung
-for (int i = 0; i < a; ++i){
-  for (int j = 0; j < b; ++j){
-    phialt[i][j][0] = 1000;  
-  }
-}
+//  // Randbedingung
+//for (int i = 0; i < a; ++i){
+//  for (int j = 0; j < b; ++j){
+//    phialt[i][j][0] = 1000;  
+//  }
+//}
     
 
  // Zeitschritte
 
-for (int t = 0; t < 100000; ++t){
+//for (int t = 0; t < 100000; ++t){
 
 //Raumelicher Loop, gehe Gitterpunkte durch
-  for (int i = 1; i < a-1; ++i){
-    for (int j = 1; j < b-1; ++j){
-     for (int k = 1; k < c-1; ++k){
-      phineu[i][j][k] = (1.0/6.0)*(phialt[i+1][j][k] + phialt[i-1][j][k] + phialt[i][j+1][k] + phialt[i][j-1][k] + phialt[i][j][k+1] + phialt[i][j][k-1]);
-      phialt[i][j][k] = phineu[i][j][k];
-      t = t+1;
-      }
-    }
-  }
-}
+//  for (int i = 1; i < a-1; ++i){
+//    for (int j = 1; j < b-1; ++j){
+//     for (int k = 1; k < c-1; ++k){
+//      phineu[i][j][k] = (1.0/6.0)*(phialt[i+1][j][k] + phialt[i-1][j][k] + phialt[i][j+1][k] + phialt[i][j-1][k] + phialt[i][j][k+1] + phialt[i][j][k-1]);
+//      phialt[i][j][k] = phineu[i][j][k];
+//      
+//      }
+//    }
+//  }
+//  t=t+1;
+//}
 
 
 
 
 // Ausgabe der Daten
-ofstream output_potential0("Potential_rho0.txt");
-  for (int i = 0; i < a; ++i){
-      for (int k = 0; k < b; ++k){
-      output_potential0 << i <<" "<< k <<" "<<phineu[i][10][k]<< endl;
-      }
-  }
+//ofstream output_potential0("Potential_rho0.txt");
+//  for (int i = 0; i < a; ++i){
+//      for (int k = 0; k < b; ++k){
+//      output_potential0 << i <<" "<< k <<" "<<phineu[i][10][k]<< endl;
+//      }
+//  }
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /////////////////Punktladung mit fester Ladungsdichte//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -64,34 +65,25 @@ ofstream output_potential0("Potential_rho0.txt");
 // Randbedingungen 
 for(int i = 0; i < a ; i++){
   for( int j = 0; j < b; j++){
-    for( int k = 0; k < c; k++){
-       if(i == 0){
+     for( int k = 0; k < c; k++){
+       if(k == 0){
 	phi2alt[i][j][k] = 1000;
+	}
+       if (i == 10 && j == 10 && k == 10){
+	 phi2alt[i][j][k] = 1000;
        }
-       if(i == 10 && j == 10 && k == 10){
-	phi2alt[i][j][k] = 1000;
-       }	 
-       else{
-       phi2alt[i][j][k] = 0.0;
-       }
-    }
+     }
   }
 }
-
 
 // Ladungsdichte definieren
 for(int i = 0; i < a; i++){
  for(int j = 0; j < b; j++){
    for(int k= 0; k < c; k++){
-     if( i == 10 && j == 10 && k == 10){
-     rhogitter[i][j][k] = 1000;
-     }
-     else{
-     rhogitter[i][j][k] = 0.0;
-     }
-   }  
- }
-} 
+      rhogitter[10][10][10] = 1000;
+    }
+  }
+}
 
 
  // Zeitschritte durch Abbruchbedingung
@@ -100,12 +92,12 @@ for (int t = 0; t < 10000; ++t){
   for (int i = 1; i < a-1; ++i){
     for (int j = 1; j < b-1; ++j){
      for (int k = 1; k < c-1; ++k){
-      phi2neu[i][j][k] =pow(pow((1.0/6.0)*(phi2alt[i+1][j][k] + phi2alt[i-1][j][k] + phi2alt[i][j+1][k] + phi2alt[i][j-1][k] + phi2alt[i][j][k+1] + phi2alt[i][j][k-1] - rhogitter[i][j][k]),2),0.5);
+      phi2neu[i][j][k] =(1.0/6.0)*(phi2alt[i+1][j][k] + phi2alt[i-1][j][k] + phi2alt[i][j+1][k] + phi2alt[i][j-1][k] + phi2alt[i][j][k+1] + phi2alt[i][j][k-1] - rhogitter[i][j][k]);
       phi2alt[i][j][k] = phi2neu[i][j][k];
-      t = t+1;
       }
     }
   }
+  t = t+1;
 }
 
 
@@ -118,8 +110,5 @@ ofstream output_potential1("Potential_punktladung.txt");
       output_potential1 << i <<" "<< k <<" "<<phineu[i][10][k]<< endl;
       }
   }
-
-
-
 
 }
